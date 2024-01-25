5f45aebf69db699f57583f47559b15544a505ace
originalrameshwar <originalrameshwar@gmail.com>
2024-01-25 21:55:34 +0530

    add some basic animations using Framer Motions Packege in Home Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,14 +11,10 @@ import figureFive from "/assets/Home/divSecond/figure-1.png";
 import figureSix from "/assets/Home/divSecond/figure-2.png";
 import appleBlackLogo from "/assets/Home/appleblacklogo.png";
 import CarouselHome from "./CarouselHome";
+import { motion, MotionConfig } from "framer-motion";
 
 const Home = () => {
-
-  const slides = [
-    <div>Slide 1</div>,
-    <div>Slide 2</div>,
-    <div>Slide 3</div>
-  ];
+  const slides = [<div>Slide 1</div>, <div>Slide 2</div>, <div>Slide 3</div>];
   return (
     <>
       <section className="bg-[#F5F5F7]">
@@ -33,19 +29,28 @@ const Home = () => {
             }}
             className="h-screen text-white text-center pt-20 shadow-2xl"
           >
-            <h1 className="text-5xl font-bold">Apple Shopping Event</h1>
-            <h2 className="mt-4 mb-3 text-gray-500 leading-tight">
-              From November 25 to 28, get an Apple Gift <br /> Card when you buy
-              an eligible product.1
-            </h2>
-            <Link className="text-[#2997FF] " to="">
-              Get an early look
-            </Link>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}
+            >
+              <h1 className="text-5xl font-bold">Apple Shopping Event</h1>
+              <h2 className="mt-4 mb-3 text-gray-500 leading-tight">
+                From November 25 to 28, get an Apple Gift <br /> Card when you
+                buy an eligible product.1
+              </h2>
+              <Link className="text-[#2997FF] " to="">
+                Get an early look
+              </Link>
+            </motion.div>
           </div>
           {/* bg-1 */}
 
           {/* bg-2 */}
-          <div
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
             style={{
               backgroundImage: `url(${bgTwo})`,
               backgroundSize: "cover",
@@ -53,12 +58,17 @@ const Home = () => {
               backgroundRepeat: "no-repeat",
             }}
             className="h-screen text-white text-center my-6 shadow-2xl mb-6"
-          ></div>
+          ></motion.div>
           {/* bg-2 */}
 
           {/* bg-3 Iphone Sections starts here */}
           <div className=" text-center shadow-xl bg-[#fbfbfa] items-center justify-center flex flex-col pt-20 mb-6">
-            <div className="pb-9">
+            <motion.div
+              initial={{ opacity: 0, y: -50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}
+              className="pb-9"
+            >
               <h1 className="text-5xl font-bold">iPhone 15</h1>
               <h2 className="mt-4 mb-3 text-gray-500 leading-tight">
                 Big and bigger.
@@ -66,14 +76,26 @@ const Home = () => {
               <Link className="text-[#2997FF] " to="">
                 Learn more &nbsp; &nbsp; Buy
               </Link>
-            </div>
-            <img src={bgThree} alt="" />
+            </motion.div>
+            <motion.img
+              initial={{ opacity: 0, x: 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              src={bgThree}
+              alt=""
+            />
           </div>
           {/* bg-3 Iphone Sections starts here */}
 
           {/* bg-4 Grid starts here */}
-          <div className="h-auto shadow-xl bg-white mb-6 grid grid-cols-2 gap-3 p-3">
-            <div className="relative shadow-md">
+          <div className="h-auto shadow-xl bg-white mb-6 grid grid-cols-2 gap-5 p-5">
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              whileHover={{ scale: 1.03 }}
+              className="relative shadow-md"
+            >
               <img className="z-0 w-[49vw] rounded" src={figureOne} alt="" />
               <div className="z-10 absolute inset-0 flex flex-col text-center -translate-y-40 justify-center items-center text-white">
                 <h3 className="text-3xl font-bold">iPhone 15 Pro</h3>
@@ -84,9 +106,15 @@ const Home = () => {
                   Learn more
                 </Link>
               </div>
-            </div>
+            </motion.div>
 
-            <div className="relative shadow-md">
+            <motion.div
+              initial={{ opacity: 0, x: 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              whileHover={{ scale: 1.03 }}
+              className="relative shadow-md"
+            >
               <img className="z-0 w-[49vw] rounded" src={figureTwo} alt="" />
               <div className="z-10 absolute inset-0 flex flex-col text-center -translate-y-40 justify-center items-center text-white">
                 <h3 className="text-black text-3xl font-bold">iPad</h3>
@@ -97,9 +125,15 @@ const Home = () => {
                   Learn more
                 </Link>
               </div>
-            </div>
+            </motion.div>
 
-            <div className="relative shadow-md">
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              whileHover={{ scale: 1.03 }}
+              className="relative shadow-md"
+            >
               <img className="z-0 w-[49vw] rounded" src={figureThree} alt="" />
               <div className="z-10 absolute inset-0 flex flex-col text-center -translate-y-40 justify-center items-center text-white">
                 <h3 className="text-black text-3xl font-bold">AirPods</h3>
@@ -110,9 +144,15 @@ const Home = () => {
                   Shop
                 </Link>
               </div>
-            </div>
+            </motion.div>
 
-            <div className="relative shadow-md">
+            <motion.div
+              initial={{ opacity: 0, x: 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              whileHover={{ scale: 1.03 }}
+              className="relative shadow-md"
+            >
               <img className="z-0 w-[49vw] rounded" src={figureFour} alt="" />
               <div className="z-10 absolute inset-0 flex flex-col text-center -translate-y-40 justify-center items-center text-white">
                 <h3 className="text-3xl font-bold">Apple Watch Series 8</h3>
@@ -123,23 +163,34 @@ const Home = () => {
                   Learn more
                 </Link>
               </div>
-            </div>
+            </motion.div>
           </div>
           {/* bg-4 Grid ends here */}
 
           {/* bg-5 Grid ends here */}
           <div className=" h-auto shadow-xl bg-white mb-6 grid grid-cols-2 gap-3 p-3">
-            <div className="relative">
+            <motion.div
+              initial={{ opacity: 0, x: -50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              whileHover={{ scale: 1.03 }}
+              className="relative"
+            >
               <img className="z-0 w-[49vw] rounded" src={figureFive} alt="" />
               <div className="z-10 absolute inset-0 flex flex-col text-center -translate-y-40 justify-center items-center">
-                <h3 className="text-3xl font-bold flex items-center mb-2">
+                <motion.h3
+                  initial={{ opacity: 0, y: 50 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1 }}
+                  className="text-3xl font-bold flex items-center mb-2"
+                >
                   <img
                     className="inline w-[40px]"
                     src={appleBlackLogo}
                     alt=""
                   />
                   WATCH
-                </h3>
+                </motion.h3>
                 <Link className="text-gray-400" to="">
                   Adventure awaits.
                 </Link>
@@ -147,18 +198,29 @@ const Home = () => {
                   Learn more
                 </Link>
               </div>
-            </div>
-            <div className="relative">
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              whileHover={{ scale: 1.03 }}
+              className="relative"
+            >
               <img className="z-0 w-[49vw] rounded" src={figureSix} alt="" />
               <div className="z-10 absolute inset-0 flex flex-col text-center -translate-y-40 justify-center items-center">
-                <h3 className="mb-2 text-3xl font-bold flex items-center">
+                <motion.h3
+                  initial={{ opacity: 0, y: 50 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1 }}
+                  className="mb-2 text-3xl font-bold flex items-center"
+                >
                   <img
                     className="inline w-[40px]"
                     src={appleBlackLogo}
                     alt=""
                   />
                   Card
-                </h3>
+                </motion.h3>
                 <Link className="text-gray-400" to="">
                   Get up to 3% Daily Cash back with every purchase.
                 </Link>
@@ -166,13 +228,13 @@ const Home = () => {
                   Learn more
                 </Link>
               </div>
-            </div>
+            </motion.div>
           </div>
           {/* bg-5 Grid ends here */}
         </div>
 
         <div className="px-4 flex justify-center shadow-lg bg-white">
-        <CarouselHome />
+          <CarouselHome />
         </div>
       </section>
     </>
