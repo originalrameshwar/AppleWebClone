f2194493d0ab6638aae328b5589683171e8ef02d
originalrameshwar <originalrameshwar@gmail.com>
2024-01-26 22:49:53 +0530

    creat Products file and add products imgs to store dir

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import Products from "./Products";
 
 const Store = () => {
   return (
-    <div>StorePage</div>
-  )
-}
+    <>
+      <section className="pt-20">
+        <div className="px-40 py10">
+          <div className="mb-10">
+            <h1 className="text-[#1D1D1F] text-3xl max-w-96">
+              <b>Store.</b>{" "}
+              <span className="text-[#6E6E73]">
+                The best way to buy the products you love.
+              </span>
+            </h1>
+          </div>
 
-export default Store
+
+          <div>
+            <Products />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Store;
